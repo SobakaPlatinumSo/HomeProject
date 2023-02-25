e764a74f7c704d6a2f34cf9703ce9154d51dd7f5
Maks <maksimbalashov08@gmail.com>
2023-02-25 21:19:08 +0700

    AirConditionBoard_gets_data_from_file

--- Home_project/ACboard_app/models.py
+++ Home_project/ACboard_app/models.py
@@ -6,5 +6,3 @@ class AirCondition:
         self.humidity = humidity
         self.CO2 = CO2
         self.created = created
-
-Air_Condition = AirCondition(temperature='2', humidity='1')
--- Home_project/ACboard_app/views.py
+++ Home_project/ACboard_app/views.py
@@ -1,12 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.renderers import JSONRenderer
-from .models import Air_Condition, AirCondition
+from .models import AirCondition
 from .serializers import AirConditionRequest
 from django.http import HttpResponse
 
 
 def AirConditionViews(request):
-	serializer = AirConditionRequest(Air_Condition)
-	json = JSONRenderer().render(serializer.data)
-	response = json
-	return 	HttpResponse(response)
+	with open('F:\Project\HomeProject\Home_project\ACboard_app\CurrentAirCondition.txt', 'r') as f:
+		Air_Condition = AirCondition(f.readline().strip(), f.readline())
+		serializer = AirConditionRequest(Air_Condition)
+		json = JSONRenderer().render(serializer.data)
+		response = json
+		return 	HttpResponse(response)
